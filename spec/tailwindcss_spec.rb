53be150edd9de5a3bf47178a0bdfb4f1252471b6
dancingball <130817704+dancingball@users.noreply.github.com>
2023-08-15 02:16:36 -0400

    Update tailwindcss_spec.rb

--- spec/tailwindcss_spec.rb
+++ spec/tailwindcss_spec.rb
@@ -2,7 +2,6 @@ RSpec.describe Tailwindcss do
   it "has a version number" do
     expect(Tailwindcss::VERSION).not_to be nil
   end
-
   it "does something useful" do
     expect(false).to eq(true)
   end
